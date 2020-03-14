336f7d13765067cbc41c89fec67b42fed311ef2b
xingyuCheng <cxy@123.com>
2020-03-14 12:58:48 +0800

    Merge branch 'master' of github.com:cxy1995123/Tools
    # Conflicts:
    #	build.gradle

--- app/src/main/java/chen/com/tools/activity/MainActivity.java
+++ app/src/main/java/chen/com/tools/activity/MainActivity.java
@@ -4,24 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
-import java.util.Calendar;
-import java.util.Date;
-
-import chen.com.library.data.TimeDate;
 import chen.com.library.systembar.StatusBarCompat;
 
 import chen.com.tools.R;
-import chen.com.tools.fragment.DocumentManagerFragment;
-import chen.com.tools.fragment.ExoPlayerFragment;
-import chen.com.tools.fragment.ExpendList;
+
 
 public class MainActivity extends AppCompatActivity {
 
