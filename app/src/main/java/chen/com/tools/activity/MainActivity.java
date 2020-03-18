7c9630fcd6cb9dc3a0a12f36b21dc7e8a777bac6
chenxingyu <cxy@123.com>
2020-03-18 14:26:10 +0800

    20200318-2

--- app/src/main/java/chen/com/tools/activity/MainActivity.java
+++ app/src/main/java/chen/com/tools/activity/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         StatusBarCompat.translucentStatusBar(this, true);
-
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
 
 
@@ -40,12 +39,7 @@ public class MainActivity extends AppCompatActivity{
 
 
     public void openCameraActivity(View view) {
-        MVideoPlayerView myVideoPlayerView = findViewById(R.id.myVideoPlayerView);
-        String url = "https://vdept.bdstatic.com/326e666a6b37587437504e61514e596c/" +
-                "384e696732635249/9342b026175c74bfb337c430327c443e57c602e81021249d7f25" +
-                "327444a5e5635fc59f89651a6134033a9d5e8a7c0348.mp4" +
-                "?auth_key=1584178560-0-0-0960cbb63513f5e925e7fb7369fa629a";
-        myVideoPlayerView.play("https://x.wuxoo4.com/mp4/f29fef11df2d42a3960d4ba0b553737f.mp4");
+
     }
 
 
